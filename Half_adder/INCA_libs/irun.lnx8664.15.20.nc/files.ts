211c54a5b41902e92d830bc5dd7dd9d5f5079a03
Rajeshwari Hipparagi <60052975+raji22082001@users.noreply.github.com>
2022-04-04 09:49:41 -0700

    Add files via upload

---
+++ Half_adder/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1646977112 /home/adld7/cds.lib
+1643351359 /home/adld7/hdl.var
+1638269400 /home/adld7/rajeshwari/Half_adder/half_adder_tb.v
+1638269262 /home/adld7/rajeshwari/Half_adder/half_adder.v
---
+++ Half_adder/half_adder_tb.v
@@ -0,0 +1,29 @@
+module half_adder_tb();
+reg a,b;
+wire sum,carry;
+half_adder DUT(.a(a),.b(b),.sum(sum),.carry(carry));
+initial begin
+$monitor("a=%b b=%b sum=%b carry=%b",a,b,sum,carry);
+a=0;
+b=0;
+#10;
+a=0;
+b=1;
+#10;
+a=1;
+b=0;
+#10;
+a=1;
+b=1;
+#10;
+end
+endmodule
+
+
+module half_adder(a,b,sum,carry);
+input a,b;
+output sum,carry;
+assign sum=a^b;
+assign carry=a&b;
+endmodule
+
