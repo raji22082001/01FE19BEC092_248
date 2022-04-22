a253aaa4ac32aeddd0e661f8b1efce8c1cccd03f
Rajeshwari Hipparagi <60052975+raji22082001@users.noreply.github.com>
2022-04-22 20:18:53 +0530

    Add files via upload

---
+++ course_project/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Priya/Desktop/Project/new_uart/uart_test_isim_beh.exe" -prj "C:/Users/Priya/Desktop/Project/new_uart/uart_test_beh.prj" "work.uart_test" "work.glbl" 
